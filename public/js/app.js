b5b4cb6d50b335cacac32c2fd0b8ec75b8a6303e
Artem Kashin <dev.tema@gmail.com>
2015-04-21 23:05:28 +0300

    слайд услуг

--- public/js/app.js
+++ public/js/app.js
@@ -250,78 +250,96 @@
 }(jQuery, window, document));
 
 (function ($, window, document) {
-
   $(function () {
 
-    blockPosition();
+    var content = $('.content-holder')
+      , contentHeight = content.height()
+      , servicesBlock = $('.services-do')
+      , sectionServices = $('.section-services')
+      , servicesHeight = sectionServices.outerHeight() * .75
+      , serviceSize = servicesHeight * .18
+      , mainSize = servicesHeight * .28;
 
-    function blockPosition() {
-      var content = $('.content-holder')
-        , contentHeight = content.height()
-        , services = $('.services-do')
-        , servicesHeight = services.outerHeight();
+    $.ajax({
+      type: 'post',
+      url: '/services',
+      data: {
+        mainSize: mainSize,
+        serviceSize: serviceSize
+      },
+      success: function (services) {
+        servicesInit(services);
+        window.onresize = function () {
+          servicesInit(services);
+        };
+      }
+    });
 
-      services.css('top', 20 + contentHeight / 2 + 'px');
-      services.css('margin-top', '-' + servicesHeight / 2 + 'px');
+    function servicesInit(services) {
 
-      window.onresize = function () {
-        blockPosition();
-      };
+      servicesBlock.css('height', servicesHeight + 'px');
+      servicesBlock.css('top', 20 + contentHeight / 2 + 'px');
+      servicesBlock.css('margin-top', '-' + servicesHeight / 2 + 'px');
+
+      $.each(services, function (i, service) {
+        servicesBlock.append(getService(service, true));
+        var serviceClass = $('.' + service.class.split(' ')[0]);
+        serviceClass.on('click', function () {
+          var $this = $(this);
+          getCurObj(services, 'class', $this.data('title'), function (obj) {
+            var itemsClass = '.' + obj.itemsClass.split(' ')[0];
+            changePosition(services, $this, function (toggle) {
+              if (toggle) {
+                $('.services-title p').text(obj.description);
+                $('.services-description').html(obj.content);
+                servicesBlock.find(itemsClass).fadeIn();
+              }
+              else {
+                servicesBlock.find(itemsClass).fadeOut();
+              }
+            });
+          });
+        });
+      });
     }
 
-    var doDigital = $('.digital-do')
-      , doMarketing = $('.marketing-do')
-      , doWeb = $('.web-do')
-      , doDesign = $('.design-do')
-      , doClient = $('.client-do')
-      , doSell = $('.sell-do')
-      , doLoyal = $('.loyal-do');
-
-    doMarketing.on('click', function () {
-      changePosition($(this), {top: '30%', left: '23%'}, function (toggle) {
-        if (toggle) {
-          $('.services-title p').text('как оптимизировать затраты на интернет-маркетинг?');
-          $('.marketing-service').fadeIn();
-        }
-        else {
-          $('.services-title p').text('мы делаем анализ вашего бизнеса, внедряем технологии, повышаем эффективность');
-          $('.marketing-service').fadeOut();
-        }
-      });
-    });
-
-    doWeb.on('click', function () {
-      changePosition($(this), {top: '0', left: '50%'});
-    });
-
-    doDesign.on('click', function () {
-      changePosition($(this), {top: '30%', left: '77%'});
-    });
-
-    doClient.on('click', function () {
-      changePosition($(this), {top: '100%', left: '50%'});
-    });
-
-    doSell.on('click', function () {
-      changePosition($(this), {top: '70%', left: '23%'});
-    });
-
-    doLoyal.on('click', function () {
-      changePosition($(this), {top: '70%', left: '77%'});
-    });
-
-    function changePosition(element, defaultPosition, cb) {
+    function changePosition(services, element, cb) {
       if (element.hasClass('active')) {
-        element.removeClass('active').animate(defaultPosition, function () {
-          element.removeClass('do-big');
-          element.siblings().removeClass('bounceOutRight').addClass('bounceInRight');
-          if (cb && typeof(cb) === "function") {
-            cb();
-          }
+
+        getCurObj(services, 'class', element.data('title'), function (obj) {
+          var size = obj.size ? obj.size : serviceSize + 'px'
+            , marginLeft = obj.marginLeft ? obj.marginLeft : '-' + serviceSize / 2 + 'px'
+            , paddingTop = obj.paddingTop ? obj.paddingTop : serviceSize / 5 + 'px'
+            , fontSize = obj.fontSize ? obj.fontSize : serviceSize / 10 + 'px'
+            , iconSize = obj.iconSize ? obj.iconSize : serviceSize / 3 + 'px';
+
+          $('.services-title p').text(services[0].description);
+          element.removeClass('active').animate({top: obj.top, left: obj.left}, function () {
+            element.css({'width': size});
+            element.css({'height': size});
+            element.css({'margin-top': obj.marginTop});
+            element.css({'margin-left': marginLeft});
+            element.css({'padding-top': paddingTop});
+            element.find('strong').css({'font-size': fontSize});
+            element.find('.icon i').css({'font-size': iconSize});
+            element.siblings().removeClass('bounceOutRight').addClass('bounceInRight');
+          });
         });
+
+        if (cb && typeof(cb) === "function") {
+          cb();
+        }
       } else {
         element.addClass('active').animate({left: '50%', top: '50%'}, function () {
-          element.addClass('do-big');
+
+          element.css({'width': mainSize + 'px'});
+          element.css({'height': mainSize + 'px'});
+          element.css({'margin-top': '-' + mainSize / 2 + 'px'});
+          element.css({'margin-left': '-' + mainSize / 2 + 'px'});
+          element.css({'padding-top': mainSize / 5 + 'px'});
+          element.find('strong').css({'font-size': mainSize / 10 + 'px'});
+          element.find('.icon i').css({'font-size': mainSize / 3 + 'px'});
+
           if (cb && typeof(cb) === "function") {
             cb(true);
           }
@@ -330,6 +348,42 @@
       }
     }
 
-  });
+    function getService(service, animation) {
+      var title = service.title.toUpperCase().replace(/ /g, '<br>')
+        , size = service.size ? service.size : serviceSize + 'px'
+        , marginLeft = service.marginLeft ? service.marginLeft : '-' + serviceSize / 2 + 'px'
+        , paddingTop = service.paddingTop ? service.paddingTop : serviceSize / 5 + 'px'
+        , fontSize = service.fontSize ? service.fontSize : serviceSize / 10 + 'px'
+        , iconSize = service.iconSize ? service.iconSize : serviceSize / 3 + 'px'
+        , animationClass = animation ? 'do wow bounceIn ' : '';
+      var newObj = '<div style="top: ' + service.top + '; left: ' + service.left + '; width: ' + size + '; height: ' + size + '; margin-top: ' + service.marginTop + '; margin-left: ' + marginLeft + '; padding-top: ' + paddingTop + ';" class="' + animationClass + service.class + '" data-title="' + service.class + '">' +
+        '<span class="icon"><i style="font-size: ' + iconSize + '" class="' + service.icon + '"></i></span>' +
+        '<strong style="font-size: ' + fontSize + '">' + title + '</strong>' +
+        '</div>';
 
+      if (service.items && service.items.length) {
+        newObj += '<div class="' + service.itemsClass + '">';
+        $.each(service.items, function (i, serv) {
+          newObj += getService(serv);
+        });
+        newObj += '<div class="services-description"></div>';
+        newObj += '</div>';
+      }
+
+      return newObj;
+    }
+
+    function getCurObj(givenArray, givenKey, givenValue, cb) {
+      $.each(givenArray, function (index, obj) {
+        $.each(obj, function (key, value) {
+          if (key == givenKey && value == givenValue) {
+            if (cb && typeof(cb) === "function") {
+              return cb(obj);
+            }
+          }
+        });
+      });
+    }
+
+  });
 }(jQuery, window, document));
--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,179 @@ router.get('/profile', function (req, res, next) {
   }
 });
 
+router.post('/services', function (req, res, next) {
+
+  var mainSize = req.body.mainSize
+    , serviceSize = req.body.serviceSize;
+
+  var services = [
+    {
+      title: 'Digital стратегия',
+      description: 'Мы делаем анализ вашего бизнеса, внедряем технологии, повышаем эффективность',
+      icon: 'pe-7s-way',
+      class: 'digital-do',
+      top: '50%',
+      left: '50%',
+      size: mainSize + 'px',
+      marginTop: '-' + mainSize / 2 + 'px',
+      marginLeft: '-' + mainSize / 2 + 'px',
+      paddingTop: mainSize / 5 + 'px',
+      fontSize: mainSize / 10 + 'px',
+      iconSize: mainSize / 3 + 'px',
+      main: true
+    },
+    {
+      title: 'Интернет маркетинг',
+      description: 'Как оптимизировать затраты на интернет-маркетинг?',
+      content: '<p>Как оптимизировать затраты на интернет-маркетинг?</p>',
+      icon: 'pe-7s-display1',
+      class: 'marketing-do',
+      top: '25%',
+      left: '23%',
+      marginTop: '0',
+      itemsClass: 'marketing-service sub-services',
+      items: [
+        {
+          title: 'Поисковое продвижение',
+          icon: 'pe-7s-rocket',
+          class: 'marketing-one do service-invert',
+          top: '25%',
+          left: '23%',
+          marginTop: '0'
+        },
+        {
+          title: 'Рекламные кампании',
+          icon: 'pe-7s-graph',
+          class: 'marketing-two do service-invert',
+          top: '2%',
+          left: '50%',
+          marginTop: '0'
+        },
+        {
+          title: 'Оптимизация конверсий',
+          icon: 'pe-7s-filter',
+          class: 'marketing-three do service-invert',
+          top: '25%',
+          left: '77%',
+          marginTop: '0'
+        }
+      ]
+    },
+    {
+      title: 'Веб продакшн',
+      description: 'Текст про веб-продакшн',
+      content: '<p><strong>Лэндинг</strong> или посадочная страница (от англ. landing page), также именуемая как «целевая страница», «страница приземления» или «страница захвата», — это веб-страница, на которую целевой посетитель попадает по рекламе или другой ссылке и которая максимально упрощает совершение им необходимого действия — оформления заказа, регистрации на сайте, подписки на рассылку.</p>',
+      icon: 'pe-7s-browser',
+      class: 'web-do',
+      top: '2%',
+      left: '50%',
+      marginTop: '0',
+      itemsClass: 'web-service sub-services',
+      items: [
+        {
+          title: 'Разработка лендингов',
+          icon: 'pe-7s-rocket',
+          class: 'web-one do service-invert',
+          top: '25%',
+          left: '23%',
+          marginTop: '0'
+        },
+        {
+          title: 'Создание сайтов',
+          icon: 'pe-7s-graph',
+          class: 'web-two do service-invert',
+          top: '2%',
+          left: '50%',
+          marginTop: '0'
+        },
+        {
+          title: 'Интернет-магазины',
+          icon: 'pe-7s-filter',
+          class: 'web-three do service-invert',
+          top: '25%',
+          left: '77%',
+          marginTop: '0'
+        }
+      ]
+    },
+    {
+      title: 'Графический дизайн',
+      description: 'Текст про дизайн',
+      content: '<p>Текст про дизайн</p>',
+      icon: 'pe-7s-pen',
+      class: 'design-do',
+      top: '25%',
+      left: '77%',
+      marginTop: '0',
+      itemsClass: 'design-service sub-services',
+      items: [
+        {
+          title: 'Дизайн сайта',
+          icon: 'pe-7s-rocket',
+          class: 'design-one do service-invert',
+          top: '25%',
+          left: '23%',
+          marginTop: '0'
+        },
+        {
+          title: 'Печатная продукция',
+          icon: 'pe-7s-graph',
+          class: 'design-two do service-invert',
+          top: '2%',
+          left: '50%',
+          marginTop: '0'
+        },
+        {
+          title: '3D модели',
+          icon: 'pe-7s-filter',
+          class: 'design-three do service-invert',
+          top: '25%',
+          left: '77%',
+          marginTop: '0'
+        }
+      ]
+    },
+    {
+      title: 'Новые клиенты',
+      description: 'Текст про клиентов',
+      content: '<p>Текст про клиентов</p>',
+      icon: 'pe-7s-users',
+      class: 'client-do service-invert',
+      top: '75%',
+      left: '23%',
+      marginTop: '-' + serviceSize + 'px',
+      itemsClass: 'client-service sub-services',
+      items: []
+    },
+    {
+      title: 'Рост продаж',
+      description: 'Текст про рост продаж',
+      content: '<p>Текст про рост продаж</p>',
+      icon: 'pe-7s-graph1',
+      class: 'sell-do service-invert',
+      top: '98%',
+      left: '50%',
+      marginTop: '-' + serviceSize + 'px',
+      itemsClass: 'sell-service sub-services',
+      items: []
+    },
+    {
+      title: 'Повышение лояльности',
+      description: 'Текст про лояльность',
+      content: '<p>Текст про лояльность</p>',
+      icon: 'pe-7s-smile',
+      class: 'loyal-do service-invert',
+      top: '75%',
+      left: '77%',
+      marginTop: '-' + serviceSize + 'px',
+      itemsClass: 'loyal-service sub-services',
+      items: []
+    }
+  ];
+
+  res.send(services);
+});
+
 router.get('/login', function (req, res, next) {
   if (!req.user) {
     if (req.xhr) {
